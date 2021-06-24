6383a15a9162817f04c8c6647974e583202823e9
Philos <i@philosyang.com>
2021-06-24 14:20:05 -0500

    removed unused functions from previous repo

--- backend/app/hello.py
+++ backend/app/hello.py
@@ -8,22 +8,10 @@ def home():
     ans.headers.add('Access-Control-Allow-Origin', '*')
     return ans
 
-# @app.route('/time')
-# def get_current_time():
-#     return {'time': time.time()}
-
-# @app.route('/test')
-# def test2():  # name doesn't matter much
-#     return jsonify("testtest")
-
 @app.route('/pythonText', methods=['POST'])
 def pythonText():
     # 1 get text from frontend
     ans = request.get_json()['text']
-    # print(ans[10] == '\n')
-    # print(ans[10])
-    # print(ans['text'])
-    # print(type(ans))
 
     # 2 write text to python file
     text_file = open("text.py", "w")
@@ -33,10 +21,8 @@ def pythonText():
 
     for i in ans:
         if i == '\n':
-            # print('1:',i)
             lines += '\n    '
         else:
-            # print('0:',i)
             lines += i
 
     print(lines)
